eb49d3882785a91018378e2b8d02585c939c3118
Ignacio Baptista [Desktop] <ibaptista@frba.utn.edu.ar>
2022-09-27 15:28:36 -0300

    Commit Inicial. Creado proyecto basico

---
+++ Assets/NetworkManagerUI.cs
@@ -0,0 +1,23 @@
+#region
+
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+#endregion
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField] private Button serverBtn;
+    [SerializeField] private Button hostBtn;
+    [SerializeField] private Button clientBtn;
+
+    private void Awake()
+    {
+        serverBtn.onClick.AddListener(() => NetworkManager.Singleton.StartServer());
+
+        hostBtn.onClick.AddListener(() => NetworkManager.Singleton.StartHost());
+
+        clientBtn.onClick.AddListener(() => NetworkManager.Singleton.StartClient());
+    }
+}
